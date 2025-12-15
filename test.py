4a623889c29441e0507e5c5b5c109f5398852e4a
Seamus <tianyusheng313@gmail.com>
2025-12-15 12:05:23 -0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from predict import predict
-
-test_url = "http://bilibili.com"
-lbl, p, feat = predict(test_url)
-print("URL:", test_url)
-print("Label:", lbl)
-print("Prob phishing:", p)
-print("Features:", feat)
